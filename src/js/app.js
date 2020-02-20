b4c0e6703cbcb0798a070f173d11966405e52930
Justyna <justyna.bierylo@gmail.com>
2020-02-20 12:34:15 +0100

    Changes in grid, hero and added Swiper init

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import 'core-js/stable'
 import 'regenerator-runtime/runtime'
 import svg4everybody from 'svg4everybody'
+import Swiper from 'swiper';
 
 svg4everybody()
 
@@ -13,3 +14,9 @@ const handleClick = () => {
 }
 
 hamburger.addEventListener('click', handleClick)
+
+// initialize Swiper
+var mySwiper = new Swiper('.swiper-container', {
+  speed: 400,
+  spaceBetween: 100
+})
