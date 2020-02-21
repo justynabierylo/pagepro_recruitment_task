d2e4cedf3aa11520ffe00287999bc43f9835e0c0
Justyna <justyna.bierylo@gmail.com>
2020-02-21 02:11:04 +0100

    Layout complete, obligatory styling done

--- src/js/app.js
+++ src/js/app.js
@@ -16,7 +16,7 @@ const handleClick = () => {
 hamburger.addEventListener('click', handleClick)
 
 // initialize Swiper
-var mySwiper = new Swiper('.swiper-container', {
+var mySwiper = new Swiper('#swiper-1', {
   speed: 600,
   slidesPerView: 1,
   loop: true,
@@ -25,6 +25,49 @@ var mySwiper = new Swiper('.swiper-container', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev'
+  },
+
+  breakpoints: {
+
+    768: {
+      spaceBetween: 20
+    },
+
+    1200: {
+      spaceBetween: 30
+    }
+  }
+
+})
+
+var mySwiperSecond = new Swiper('#swiper-2', {
+  speed: 600,
+  // loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+
+  slidesPerView: 1,
+  slidesPerColumnFill: 'column',
+  slidesPerColumn: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+
+    768: {
+      slidesPerColumnFill: 'row',
+
+      slidesPerView: 2
+      // spaceBetween: 20
+    },
+
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
   }
 
 })
